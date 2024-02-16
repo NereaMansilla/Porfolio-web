a382a36668014bf69d2ae860e834c237da996102
Nerea <NereaMansillaBarreiro@gmail.com>
2024-02-15 22:42:07 -0300

    add slide in Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,21 +4,61 @@ import Weather from '../../img/Weather.png'
 import Sitioweb from '../../img/Sitioweb.png'
 import { MdRemoveRedEye } from "react-icons/md";
 import { FaArrowRightLong } from "react-icons/fa6";
+
  */
+import {ProjectsJSON} from "../ProjectsJSON.js"
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Mousewheel, Pagination } from 'swiper/modules';
+
+
 export const Projects= () => {
  
-
-
-
   return (
-    <section className=" text-black  bg-[#210D42] relative  " id="Projects">
-        
+    <section className="h-full pb-20">
+    <Swiper
+      direction={'vertical'}
+      slidesPerView={1}
+      /* spaceBetween={30} */
+      mousewheel={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Mousewheel, Pagination]}
+      className="bg-[#fff] h-[30vh] w-[80vw]"
+    >
+       {ProjectsJSON.map((p)=>(
+   <SwiperSlide key={p.id}>
+     
+     <img src={p.img} alt={p.title} />
 
+   </SwiperSlide>
+    
+  ))}
+    {/*   <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide> */}
+    </Swiper>
+    
+  {/*  {ProjectsJSON.map((p)=>(
+    <div className="h-full" key={p.id}>
+      {p.title}
+      <img src={p.img} alt={p.title} />
+    </div>
+  ))}
+    */}
+  </section>
 
 
  
 
 
-    </section>
   )
 }
---
+++ src/ProjectsJSON.js
@@ -0,0 +1,29 @@
+import starwars from "../src/img/StarWars.png"
+import notedo from "../src/img/NoteDo.png"
+import books from "../src/img/SitioWeb.png"
+
+
+export const ProjectsJSON = [
+    {    id:1,
+        title:"Star Wars",
+        img:starwars,
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        technologies:""
+
+    },
+    {    id:2,
+        title:"NoteDo",
+        img:notedo,
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        technologies:""
+
+    },
+    {    id:3,
+        title:"Books Website",
+        img:books,
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        technologies:""
+
+    },
+  
+]
